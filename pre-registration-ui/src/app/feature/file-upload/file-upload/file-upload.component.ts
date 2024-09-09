0d5b40cd693f85bab21a335f3df9aa7dfba2debe
GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>
2024-09-09 16:35:36 +0530

    added proper error handling
    Signed-off-by: GOKULRAJ136 <110164849+GOKULRAJ136@users.noreply.github.com>

--- pre-registration-ui/src/app/feature/file-upload/file-upload/file-upload.component.ts
+++ pre-registration-ui/src/app/feature/file-upload/file-upload/file-upload.component.ts
@@ -1177,7 +1177,7 @@ export class FileUploadComponent implements OnInit, OnDestroy {
           } 
         },
         (error) => {
-          if (error.apiErrorCodes="PRG_PAM_DOC_025"){
+          if (error.error.errors[0].errorCode == "PRG_PAM_DOC_025"){
             this.showErrorMessage(error, this.messagelabels.uploadDocuments.msg14);
           } else {
             this.showErrorMessage(error, this.messagelabels.uploadDocuments.msg7);
